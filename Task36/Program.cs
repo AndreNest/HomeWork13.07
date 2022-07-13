b873d0a7abdd4204b73212bb3a28c2ae34964cca
AndreyNesterov <1@nestandre.ru>
2022-07-14 00:08:05 +0300

    Add task 36.1

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -4,16 +4,14 @@ Console.Write("Введите длину массива: ");
 int lenthArr = Convert.ToInt32(Console.ReadLine());
 int[] newArr = new int[lenthArr];
 int total = 0;
-for (int i = 0; i < lenthArr; i++)
+for (int i = 1; i < lenthArr; i+=2) //if(i % 2 != 0) // for (int i = 0; i < lenthArr && i % 2 != 0; i++)
 {
     newArr[i] = new Random().Next(100, 1000); // newArr[i] = rnd.Next(-9, 10);
-    Console.Write(newArr[i] + " ");
-    if(i % 2 != 0)
-    {
-         total = total + newArr[i];
-    }
+    Console.Write(newArr[i] + " "); 
+    total = total + newArr[i];
+    
     
 }
 Console.WriteLine();
 Console.WriteLine("---------------------------------");
-Console.WriteLine("Количество четных чисел в массиве: " + total);
+Console.WriteLine("Сумма нечетных значений массива " + total);
