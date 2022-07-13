26749c4be2d1186893146d651348b1325fe9d307
AndreyNesterov <1@nestandre.ru>
2022-07-14 00:52:08 +0300

    add Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,23 @@
+﻿// Task38
+
+
+double[] newArr = new double[5]{3, 7, 22, 2, 78};
+int minNumb = 0;
+int maxNumb = 0;
+double result = 0;
+for (int i = 0; i < 5; i++) 
+{
+    if(newArr[i] < newArr[minNumb])
+    {
+        minNumb = i;
+    }
+    else if(newArr[i] > newArr[maxNumb])
+    {
+        maxNumb = i;
+    }
+}
+
+result = newArr[maxNumb] - newArr[minNumb];
+Console.WriteLine();
+Console.WriteLine("---------------------------------");
+Console.WriteLine("Разница между максимальным и минимальным значением массива = " + result);
